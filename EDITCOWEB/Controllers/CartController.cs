ff69049f34167dcfac747d70f14b9d3e22bfba1f
ranakilincc <ranakilinc23@gmail.com>
2025-12-26 23:26:32 +0300

    favori sayfası siparişlerim sayfası ürün görselleri

---
+++ EDITCOWEB/Controllers/CartController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EDITCOWEB.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            // KURAL: Eğer kullanıcı giriş yapmamışsa, Giriş Yap sayfasına gönder.
+            if (HttpContext.Session.GetString("UserEmail") == null)
+            {
+                return Redirect("/Account/Login");
+            }
+
+            // Giriş yapmışsa Sepet sayfasını göster.
+            return View();
+        }
+    }
+}
